995f5df07df49f078c5ecf832fb1c7c501032a12
Taruna-Patil <tarunapatil372@gmail.com>
2025-10-27 17:10:44 +0530

    first Commit

---
+++ sys_customization/sys_customization/doctype/registrations/registrations.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Quntbit Technologies pvt ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Registrations", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sys_customization/sys_customization/doctype/registrations/test_registrations.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Quntbit Technologies pvt ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRegistrations(FrappeTestCase):
+	pass
